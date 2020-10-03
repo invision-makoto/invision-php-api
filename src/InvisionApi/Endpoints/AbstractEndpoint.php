d2d262c1d917ba249c64ec5b985aec745f21eb80
Makoto <makoto+github@taiga.sh>
2020-10-02 21:39:05 -0400

    Abstract endpoint class

---
+++ src/InvisionApi/Endpoints/AbstractEndpoint.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace InvisionApi\Endpoints;
+
+abstract class AbstractEndpoint
+{
+    /**
+     * Invisian API client
+     * @var \InvisionApi\Api
+     */
+    protected $api;
+
+    /**
+     * HTTP client for the abstract endpoint
+     * @var \GuzzleHttp\Client
+     */
+    protected $client;
+
+    /**
+     * Invision API endpoint interface
+     * @param \InvisionApi\Api $api
+     * @return void
+     */
+    public function __construct( \InvisionApi\Api $api )
+    {
+        $this->api = $api;
+    }
+}
